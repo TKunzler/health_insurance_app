9a0eb2ac774919f75b78d3f836a7874564aea6a1
Thomas Kunzler <thomaskz@hotmail.com>
2025-03-26 18:27:09 -0300

    First Commit

---
+++ handler.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+from healthinsurance import HealthInsurance
+
+# Load the model
+model = pickle.load(open( 'models/model_health_insurance.pkl', ''rb ) )
+
+# Initialize API
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def health_insurance_predict():
+    test_json = request.get_json()
+
+    if test_json:  # There is data
+        if isinstance(test_json, dict):  # For a single line
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else:  # For multiple lines
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+        # Instantiate Healthcare Class
+        pipeline = HealthInsurance()
+        
+        # Feature Engineering
+        df1 = pipeline.feature_engineering( test_raw )
+    
+        # Data Preparation
+        df2 = pipeline.data_preparation( df1 )
+        
+        # Prediction
+        df_response = pipeline.get_prediction( model, test_raw, df2 )
+    
+        # Return
+        return df_response
+    
+    else:
+        return Response('{No Data}', status=200, mimetype='application/json')
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=port) 
---
+++ healthinsurance/HealthInsurance.py
@@ -0,0 +1,68 @@
+class HealthInsurance:
+
+    def __init__( self ):
+        self.home_path = ''
+        self.annual_premium_scaler =            pickle.load( open( self.home_path + 'features/encoding_annual_premium.pkl', 'rb' ) )
+        self.age_scaler =                       pickle.load( open( self.home_path + 'features/encoding_age.pkl', 'rb' ) )
+        self.vintage_scaler =                   pickle.load( open( self.home_path + 'features/encoding_vintage.pkl', 'rb' ) )
+        self.target_encode_gender_scaler =      pickle.load( open( self.home_path + 'features/encoding_gender.pkl', 'rb' ) )
+        self.target_encode_region_code_scaler = pickle.load( open( self.home_path + 'features/encoding_region_code.pkl', 'rb' ) )
+        self.fe_policy_sales_channel_scaler =   pickle.load( open( self.home_path + 'features/encoding_policy_sales_channel.pkl', 'rb' ) )
+    
+    def feature_engineering(self, df):
+        # Create Copy for this session
+        df2 = df.copy()
+        
+        # Vehicle Damage
+        df2['Vehicle_Damage'] = df2['Vehicle_Damage'].apply( lambda x: 1 if x == 'Yes' else 0 )
+        
+        # Vehicle Age
+        df2['Vehicle_Age'] =  df2['Vehicle_Age'].apply( lambda x: 'over_2_years' if x == '> 2 Years' else 'between_1_2_year' if x == '1-2 Year' else 'below_1_year' )
+        
+        return df2
+
+    
+    
+    def data_preparation(self, df2):
+        # annual premium - Standard Scaler
+        df2['Annual_Premium'] = self.annual_premium_scaller.transform(df2[['Annual_Premium']].values)
+                                                                     
+        # Age - MinMax Scaler
+        df2['Age'] = self.age_scaller.transform(df2[['Age']].values)
+
+        # Vintage - MinMax Scaler
+        df2['Vintage'] = self.vintage_scaller.transform(df2[['Vintage']].values)
+
+        # Gender - One Hot Encoding / Target Encoding
+        df2.loc[:, 'Gender'] = df2['Gender'].map(self.target_encode_gender_scaler)
+        
+        # Region_Code - Frequency Encoding / Target Encoding
+        df2.loc[:, 'Region_Code'] = df2['Region_Code'].map(self.target_encode_region_code_scaler)
+        
+        # Vehicle_Age - One Hot Encoding / Frequency Encoding
+        df2 = pd.get_dummies(df2, prefix='Vehicle_Age', columns=['Vehicle_Age'])
+        
+        # Policy_Sales_Channel - Frequency Encoding / Target Encoding
+        df2.loc[:, 'Policy_Sales_Channel'] = df2['Policy_Sales_Channel'].map(self.fe_policy_sales_channel_scaler)
+
+        # Selecting the most relevant features
+        cols_selected = ['Annual_Premium',
+                         'Vintage',
+                         'Age',
+                         'Region_Code',
+                         'Vehicle_Damage',
+                         'Previously_Insured',
+                         'Policy_Sales_Channel']
+
+        return df2[cols_selected]
+
+
+    
+    def get_prediction( self, model, original_data, test_data ):
+        # model prediction
+        pred = model.predict_proba( test_data )
+
+        # join prediction into original data
+        original_data['Score'] = pred
+
+        return original_data.to_json( orient='records', date_format='iso' )
